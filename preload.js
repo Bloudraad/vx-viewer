fb9c7f22d19d71eb78dd54478ed50da904313ac5
Stanley Nguyen <hung.ngn.the@gmail.com>
2022-01-17 18:34:34 +0800

    fix: update trex angle

--- preload.js
+++ preload.js
@@ -24,7 +24,7 @@ document.querySelector('#viewer').innerHTML = `
       style="width: 100vw; height: 100vh"
       src="https://storage.googleapis.com/cryptotrexnft/vxgltf/${rexNumber}.gltf"
       camera-controls
-      orientation="0deg 0deg 210deg"
+      orientation="0deg 0deg 150deg"
       shadow-intensity="1"
     />`;
 
